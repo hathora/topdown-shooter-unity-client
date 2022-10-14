460c173d322e727b2715fbdcb4e0c7c3587b5538
Vidur Murali <vidur.murali@gmail.com>
2022-10-14 14:25:34 -0400

    Smooth out bullet movement

--- Assets/_Source/Bullet.cs
+++ Assets/_Source/Bullet.cs
@@ -5,12 +5,26 @@ public class Bullet : MonoBehaviour {
 
     string id;
 
+    // Position to move to
+    Vector3 targetPosition;
+
+    // Used to interpolate server position
+    // with client's position to create smooth
+    // movement
+    float smoothTime = 0.05f;
+    float speed = 100;
+    Vector3 velocity;
+
     public void Init(BulletData data) {
         this.id = data.id;
+        targetPosition = transform.position;
     }
 
     public void Render(BulletData data) {
-        Vector3 pos = new Vector3(data.position.x, data.position.y);
-        transform.position = pos;
+        targetPosition = new Vector3(data.position.x, data.position.y);
+    }
+
+    void Update() {
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, speed);
     }
 }
